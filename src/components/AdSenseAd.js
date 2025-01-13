56c695885c69a5d52bfd80998cd796a462a7adfe
sharad patil <sharad>
2025-01-13 22:33:57 +0530

    new

---
+++ src/components/AdSenseAd.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+
+const AdSenseAd = ({ adClient, adSlot }) => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("AdSense error: ", e);
+    }
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", margin: "20px 0" }}>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client={adClient}
+        data-ad-slot={adSlot}
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
+  );
+};
+
+export default AdSenseAd;
