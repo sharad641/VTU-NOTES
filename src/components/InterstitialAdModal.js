c4dcde1f3f6918a9996a4f70b4084d05749d5094
Sharad Patil <sharadpatilrocks42@gmail.com>
2026-02-01 12:55:39 +0530

    Fixed Popup Ad close button positioning and improved Module Detail mobile responsiveness

--- src/components/InterstitialAdModal.js
+++ src/components/InterstitialAdModal.js
@@ -52,7 +52,7 @@ const InterstitialAdModal = ({ isOpen, onComplete, resourceTitle, isVideoAd = fa
                   <motion.button 
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
-                    className="close-modal-btn" 
+                    className="interstitial-close-btn" 
                     onClick={handleClose}
                   >
                     <HiXMark />
